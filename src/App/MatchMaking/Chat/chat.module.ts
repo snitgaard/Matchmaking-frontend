3e14ba650ccba86fc2651c83971397bce1a36c78
SerireS <jonasphp@live.dk>
2021-04-27 11:10:27 +0200

    ChatComponent

--- src/App/MatchMaking/Chat/chat.module.ts
+++ src/App/MatchMaking/Chat/chat.module.ts
@@ -2,6 +2,14 @@ import {NgModule} from '@angular/core';
 import {ChatComponent} from './chat.component';
 import {AppRoutingModule} from '../../app-routing.module';
 import {ChatRoutingModule} from './chat-routing.module';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {FlexModule} from '@angular/flex-layout';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
 
 
 
@@ -11,7 +19,15 @@ import {ChatRoutingModule} from './chat-routing.module';
   ],
 
   imports: [
-    ChatRoutingModule
+    ChatRoutingModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    FlexModule,
+    MatDividerModule,
+    MatListModule
   ]
 })
 export class ChatModule { }
