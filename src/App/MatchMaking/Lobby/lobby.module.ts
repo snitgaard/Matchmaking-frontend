9a43d5ca440d96502805ef14f307029330478ad5
SerireS <jonasphp@live.dk>
2021-04-28 11:20:21 +0200

    .

--- src/App/MatchMaking/Lobby/lobby.module.ts
+++ src/App/MatchMaking/Lobby/lobby.module.ts
@@ -1,6 +1,14 @@
 import {NgModule} from '@angular/core';
 import {LobbyComponent} from './lobby.component';
 import {LobbyRoutingModule} from './lobby-routing.module';
+import {FlexModule} from '@angular/flex-layout';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
 
 
 
@@ -10,7 +18,15 @@ import {LobbyRoutingModule} from './lobby-routing.module';
   ],
 
   imports: [
-    LobbyRoutingModule
+    LobbyRoutingModule,
+    FlexModule,
+    MatCardModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatDividerModule,
+    MatListModule,
+    MatButtonModule,
+    MatInputModule
   ]
 })
 export class LobbyModule { }
--- src/App/app-routing.module.ts
+++ src/App/app-routing.module.ts
@@ -7,6 +7,7 @@ const routes: Routes = [
   { path: 'Chat', loadChildren: () => import('./MatchMaking/Chat/chat.module').then(m => m.ChatModule) },
   { path: 'Profile', loadChildren: () => import('./MatchMaking/Profile/profile.module').then(m => m.ProfileModule)},
   { path: 'Login', loadChildren: () => import('./MatchMaking/Login/login.module').then(m => m.LoginModule) },
+  { path: 'Lobby', loadChildren: () => import('./MatchMaking/Lobby/lobby.module').then(m => m.LobbyModule) },
   { path: '**', loadChildren: () => import('./MatchMaking/Error/page-not-found/page-not-found.module').then(m => m.PageNotFoundModule) },
 ];
 @NgModule({
