308944c395877da961e173ba5ab6d1949fa647ca
FimFi <55874743+FimFi@users.noreply.github.com>
2021-05-03 14:28:04 +0200

    aaaaaaaaaaaaaaaaaaaa

--- src/App/MatchMaking/Leaderboard/leaderboard.component.ts
+++ src/App/MatchMaking/Leaderboard/leaderboard.component.ts
@@ -4,6 +4,7 @@ import {UserState} from '../Profile/state/user.state';
 import {Observable} from 'rxjs';
 import {UserModel} from '../shared/user.model';
 import {ListenForUsers} from '../Profile/state/user.actions';
+import {map, tap} from "rxjs/operators";
 
 @Component({
   selector: 'app-leaderboard',
@@ -13,11 +14,21 @@ import {ListenForUsers} from '../Profile/state/user.actions';
 export class LeaderboardComponent implements OnInit {
   @Select(UserState.users) users$: Observable<UserModel[]> | undefined;
 
+
   constructor(private store: Store) {
   }
 
   ngOnInit(): void {
     this.store.dispatch(new ListenForUsers());
+    this.sort();
   }
+  sort() {
 
+    this.users$ = this.users$.pipe(tap((data) => {
+      data.sort((a, b) => {
+        return a.rating < b.rating ? -1 : 1;
+      });
+      return data;
+    }))
+  }
 }
