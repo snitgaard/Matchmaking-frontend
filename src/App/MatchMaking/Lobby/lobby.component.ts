1b0c2a95df254ed29f380363bddba8994d8b672c
dreamy <31621059+dreamyy1@users.noreply.github.com>
2021-05-19 13:44:12 +0200

    :(

--- src/App/MatchMaking/Lobby/lobby.component.ts
+++ src/App/MatchMaking/Lobby/lobby.component.ts
@@ -5,7 +5,11 @@ import {Router} from '@angular/router';
 import {ProfileComponent} from '../Profile/profile.component';
 import {MatchModel} from '../shared/match.model';
 import {MatchState} from './state/match.state';
-import {Observable} from 'rxjs';
+import {Observable, Subject} from 'rxjs';
+import {LoginState} from '../login/state/login.state';
+import {takeUntil} from 'rxjs/operators';
+import {MatchResultsModel} from '../shared/match-results.model';
+import {ListenForMatchResults, NewMatch} from './state/match.actions';
 
 
 @Component({
@@ -17,8 +21,28 @@ import {Observable} from 'rxjs';
 export class LobbyComponent implements OnInit
 {
   @Select(MatchState.activeMatch) activeMatch$: Observable<MatchModel> | undefined;
+  @Select(MatchState.matchResults) matchResults$: Observable<MatchResultsModel[]> | undefined;
+  unsubscribe$ = new Subject();
+  relevantResults: MatchResultsModel[] = [];
   constructor(private store: Store) {}
+
   ngOnInit(): void {
+    this.store.dispatch(new ListenForMatchResults());
+    this.store.dispatch(new NewMatch());
+    this.getUsersOnMatch();
+  }
+
+  getUsersOnMatch(): void {
+    const activeMatch = {...this.store.selectSnapshot(MatchState.activeMatch)};
+    this.matchResults$.pipe(takeUntil(this.unsubscribe$)).subscribe((matchResults) => {
+      matchResults.forEach(relevantResult => {
+        const index = this.relevantResults.findIndex(result => result.id === relevantResult.id);
+        if (relevantResult.match.id === activeMatch.id && index === -1)
+        {
+          this.relevantResults.push(relevantResult);
+        }
+      });
+    });
   }
 
 }
